2bad1f7284c9633ebf12d6268074703ae8b593c1
Le Van Tang <vantangst@gmail.com>
2020-05-19 20:57:39 +0700

    integrate network part

---
+++ app/src/main/java/test/com/androidtestmvvm/data/model/api/User.kt
@@ -0,0 +1,8 @@
+package test.com.androidtestmvvm.data.model.api
+
+class User (val id: Int,
+            val email: String,
+            val name: String?,
+            val phone: String?,
+            val address: String?,
+            val access_token: String = "")
